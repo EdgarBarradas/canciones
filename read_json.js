eb791ac610881d1a854e3c7ab242db6569166ceb
EdgarBarradas <90732716+EdgarBarradas@users.noreply.github.com>
2025-10-12 15:52:55 +0200

    Update read_json.js

--- read_json.js
+++ read_json.js
@@ -2,15 +2,13 @@ async function cargarDatos(json_file) {
     const respuesta = await fetch(json_file);
     const song_data = await respuesta.json();
     return song_data;
-  }
-  
+    }
 // Luego puedes usar los datos fuera del fetch
 async function main() {
     json_file="Santo.json"
     const song_data = await cargarDatos(json_file);
     console.log(song_data); // ✅ Aquí ya puedes usar los datos
-}
-  
-
+    }
 main();
+
 
