891d4447b812c6abaa93b57b77112d3397c3ba45
EdgarBarradas <90732716+EdgarBarradas@users.noreply.github.com>
2025-10-12 15:31:02 +0200

    Add files via upload

---
+++ read_json.js
@@ -0,0 +1,13 @@
+async function cargarDatos(json_file) {
+    const respuesta = await fetch(json_file);
+    const song_data = await respuesta.json();
+    return song_data;
+  }
+  
+  // Luego puedes usar los datos fuera del fetch
+  async function main() {
+    const song_data = await cargarDatos(json_file);
+    console.log(song_data); // ✅ Aquí ya puedes usar los datos
+  }
+  
+  main();
