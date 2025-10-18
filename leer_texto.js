2c7021ff9176b7b1dad4fb2fd31e239f174cff2c
EdgarBarradas <90732716+EdgarBarradas@users.noreply.github.com>
2025-10-18 13:41:43 +0200

    Change data fetching from JSON to text format

--- leer_texto.js
+++ leer_texto.js
@@ -1,6 +1,6 @@
 async function cargarDatos(txt_file) {
     const respuesta = await fetch(txt_file);
-    const song_data = await respuesta.json();
+    const song_data = await respuesta.text();
     return song_data;
     }
 // Luego puedes usar los datos fuera del fetch
@@ -9,4 +9,5 @@ async function main() {
     const song_data = await cargarDatos(txt_file);
     console.log(song_data); // ✅ Aquí ya puedes usar los datos
     }
+
 main();
