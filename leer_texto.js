bc3c35848984565f1097f1f9fb3c63875970a435
EdgarBarradas <90732716+EdgarBarradas@users.noreply.github.com>
2025-10-18 13:46:21 +0200

    Modify cargarDatos to return text and execute data
    Added eval to execute the fetched song data.

--- leer_texto.js
+++ leer_texto.js
@@ -1,13 +1,15 @@
 async function cargarDatos(txt_file) {
     const respuesta = await fetch(txt_file);
-    const song_data = await respuesta.text();
-    return song_data;
+    //const song_data = await respuesta.text();
+    return await respuesta.text();
     }
 // Luego puedes usar los datos fuera del fetch
 async function main() {
     txt_file="console_log.txt"
     const song_data = await cargarDatos(txt_file);
     console.log(song_data); // ✅ Aquí ya puedes usar los datos
+    eval(song_data)
     }
 
 main();
+
