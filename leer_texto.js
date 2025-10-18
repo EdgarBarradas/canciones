12a3c7f34c27014a7eb478cd29fcf43207066bad
EdgarBarradas <90732716+EdgarBarradas@users.noreply.github.com>
2025-10-18 14:03:41 +0200

    Comment out leerArchivoDeTexto function

--- leer_texto.js
+++ leer_texto.js
@@ -1,16 +1,16 @@
-async function leerArchivoDeTexto(txt_file) {
-    return await (await fetch((txt_file))).text();
-    }
+// async function leerArchivoDeTexto(txt_file) {
+//     return await (await fetch((txt_file))).text();
+//     }
 
 // Luego puedes usar los datos fuera del fetch
 async function usarTexto(txt_file) {
-    // txt_file="console_log.txt"
-    contenido_txt = await leerArchivoDeTexto(txt_file);
+    contenido_txt = await (await fetch((txt_file))).text();
     console.log(contenido_txt);
     eval(contenido_txt)
     }
 
 usarTexto("console_log.txt");
+
 
 
 
