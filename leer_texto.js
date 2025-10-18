98a3c3d7dc28ba35f404828b5f857ff2afe7d035
EdgarBarradas <90732716+EdgarBarradas@users.noreply.github.com>
2025-10-18 14:18:09 +0200

    Fix global variable declaration in usarTexto function

--- leer_texto.js
+++ leer_texto.js
@@ -1,11 +1,12 @@
 async function usarTexto(txt_file) {
-    global letra_acordes = await (await fetch((txt_file))).text();
+    letra_acordes = await (await fetch((txt_file))).text();
     console.log(letra_acordes);
     eval(letra_acordes)
     console.log(letra.tonalidad)
     }
 
 usarTexto("letras/santo_m.txt");
+
 
 
 
