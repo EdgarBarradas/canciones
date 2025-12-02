fa0827ce3b841cea027c48abe3284a7f8e9bb7f3
EdgarBarradas <90732716+EdgarBarradas@users.noreply.github.com>
2025-12-02 08:33:20 +0100

    Update canciones.js

--- canciones.js
+++ canciones.js
@@ -128,9 +128,9 @@ async function leerCanciones(txt_file) {///////---- función asíncrona que lee 
         canciones_filtradas.forEach(c => {
               document.getElementById('resultados').innerHTML += `
                 <div class="cancion">
-                  <h3>${c.titulo} </h3>
-                  <h4 class="azul" onclick="mostrarLetraAcordes('${c.txt}')">Ver letra y acordes</h4>
-                  <audio controls src="${c.audio}"></audio>
+                  <h3 onclick="mostrarLetraAcordes('${c.txt}')>${c.titulo} </h3>
+                  <!--h4 class="azul" onclick="mostrarLetraAcordes('${c.txt}')">Ver letra y acordes</h4-->
+                  <!--audio controls src="${c.audio}"></audio-->
                 </div>`;
         });
 
@@ -143,6 +143,7 @@ leerCanciones("canciones.txt")
 
 
   
+
 
 
 
