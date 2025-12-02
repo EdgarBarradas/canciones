0b3711d1e38d6ddc49732f4f7ffb8ab7930283b2
EdgarBarradas <90732716+EdgarBarradas@users.noreply.github.com>
2025-12-02 09:36:49 +0100

    Update canciones.js

--- canciones.js
+++ canciones.js
@@ -106,12 +106,7 @@ async function mostrarLetraAcordes(txt_file) {
 }        
 
 async function mostrarAudioLetraAcordes(audio_file, txt_file) {
-  audio_item = '<audio controls src="'+audio_file+'"></audio>'
-  
-  console.log(audio_file)
-  console.log(audio_item)
-  
-  document.getElementById("bloque_audio").innerHTML = audio_item
+  document.getElementById("bloque_audio").innerHTML = '<audio controls src="'+audio_file+'"></audio>'
   letra_acordes = await (await fetch((txt_file))).text();
   eval(letra_acordes)
   crear_bloque_letra(letra)
@@ -153,6 +148,7 @@ leerCanciones("canciones.txt")
 
 
   
+
 
 
 
