748b52ab07bf5c1430273bf5a0c257ddcfa178cd
EdgarBarradas <90732716+EdgarBarradas@users.noreply.github.com>
2025-12-04 11:55:45 +0100

    Update canciones.js

--- canciones.js
+++ canciones.js
@@ -114,9 +114,9 @@ async function mostrarAudioLetraAcordes(audio_file, tono_audio, txt_file) {
     console.log(exist)
     if (exist) {
       document.getElementById("bloque_audio").innerHTML = '<audio controls src="'+audio_file+'"></audio>'
-      document.getElementById("bloque_audio").innerHTML += '<p >Audio por '+tono_audio+'</p>'
+      document.getElementById("bloque_audio").innerHTML += '<p style="color: grey;">Audio por '+tono_audio+'</p>'
     } else {
-      document.getElementById("bloque_audio").innerHTML += '<p >No hay audio para esta canción</p>'
+      document.getElementById("bloque_audio").innerHTML = '<p style="color: grey;">No hay audio para esta canción</p>'
     }
 });
   letra_acordes = await (await fetch((txt_file))).text();
@@ -161,6 +161,7 @@ leerCanciones("canciones.txt")
 
 
   
+
 
 
 
