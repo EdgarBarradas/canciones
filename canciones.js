77f7c087720ef2099af9632337faf01f90fdd8f2
EdgarBarradas <90732716+EdgarBarradas@users.noreply.github.com>
2025-12-11 06:57:59 +0100

    Update canciones.js

--- canciones.js
+++ canciones.js
@@ -143,9 +143,9 @@ async function listarCanciones(txt_file) {///////---- función asíncrona que le
             ( momento==="" || (c.momentos.includes(momento))) &&
             ( texto==="" || c.titulo.toLowerCase().includes(texto))
             )
-        console.log(canciones_filtradas[0:5])
+        console.log(canciones_filtradas[0])
         canciones_filtradas.sort()
-        console.log(canciones_filtradas[0:5])
+        console.log(canciones_filtradas[0])
         
         canciones_filtradas.forEach(c => {
               document.getElementById('resultados').innerHTML += `
@@ -163,6 +163,7 @@ listarCanciones("canciones.txt")
 
 
   
+
 
 
 
