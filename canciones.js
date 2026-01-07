7be60be543e538e22d5ae63eef148a441980eb60
EdgarBarradas <90732716+EdgarBarradas@users.noreply.github.com>
2026-01-07 11:39:13 +0100

    Update canciones.js

--- canciones.js
+++ canciones.js
@@ -153,7 +153,8 @@ async function buscarCanciones(canciones) {
   canciones_filtradas.sort((a,b)=>a.titulo.localeCompare(b.titulo))
 
   canciones_filtradas.forEach(c => {
-        showing_text = c.titulo+" "+showIfAudio(c.audio)+"♫"
+        simbol = await showIfAudio(c.audio)
+        showing_text = c.titulo+" "+simbol+"♫"
         document.getElementById('resultados').innerHTML += `
           <div class="cancion">
             <h3 class="result" onclick="mostrarAudioLetraAcordes('${c.audio}', '${c.tono_audio}', '${c.txt}')">${showing_text}</h3>
@@ -178,6 +179,7 @@ listarCanciones("canciones.txt")
 
 
   
+
 
 
 
